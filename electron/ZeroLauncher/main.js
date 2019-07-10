d7dcf963927c22dd8454319685690bd0580a3543
Zero-Ware <exploitfbgn@protonmail.com>
2019-07-10 18:09:58 +0200

    added electron version

---
+++ electron/ZeroLauncher/main.js
@@ -0,0 +1,31 @@
+const {
+  app,
+  BrowserWindow
+} = require('electron')
+const path = require('path')
+let mainWindow
+
+function createWindow() {
+  let mainWindow = new BrowserWindow({
+    backgroundColor: '#383838',
+    width: 900,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  // mainWindow.maximize()
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.loadFile('index.html')
+  mainWindow.on('closed', function() {
+    mainWindow = null
+
+  })
+}
+app.on('ready', createWindow)
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') app.quit()
+})
+app.on('activate', function() {
+  if (mainWindow === null) createWindow()
+})
